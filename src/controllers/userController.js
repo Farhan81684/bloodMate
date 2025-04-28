687f9ac1b82555d8724141072d18e271bec51fe3
hassanrazalashari <hassanrazalashari001@gmail.com>
2025-04-29 04:20:09 +0500

    chng

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -96,7 +96,17 @@ exports.forgotPassword = async (req, res) => {
             subject: "Password Reset",
             text: `Your OTP is ${otp}`,
         };
-        await transporter.sendMail(mailOptions);
+        await transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('Error sending email:', error);
+                return res.status(500).json({ message: "Failed to send email", error: error.message });
+            } else {
+                console.log('Email sent:', info.response);
+                res.status(200).json({ message: "OTP sent successfully", otp: otp });
+            }
+        });
+        
+    
         console.log("OTP sent to email:", email, otp);
         res.status(200).json({ message: "OTP sent successfully", otp: otp });
     } catch (error) {
